7e130f742c87967dfec431f74ee6d650bae6d262
lucas <lucas@172.16.91.99>
2019-10-08 10:41:53 -0300

    Foi criado um projeto Xadrez, e adicionando as classes Cor, Peca e Posicao.

---
+++ src/modelo/Cor.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public enum Cor {
+	
+	BRANCA, PRETA;
+
+}
---
+++ src/modelo/peca.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class peca {
+	
+	private Cor cor;
+	private Posicao posicao;
+	private boolean nuncaMovida;
+
+}
